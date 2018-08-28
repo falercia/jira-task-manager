ef73f1cb85f866118d2382ee76f919d66d14bb36
Fabio Garcia <fabio.garcia@ypse.com.br>
2018-08-28 17:24:57 -0300

    Remove routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ Route::get('/admin/indicators', function () {
 //   return view('admin.indicators');
 //});
 
-Route::get('/admin/indicators', 'API\v1\QueryController@getIndicators');
+/*Route::get('/admin/indicators', 'API\v1\QueryController@getIndicators');
 Route::get('/admin/time_tracking', 'API\v1\QueryController@getUsersTimeTracking');
 Route::get('/admin/productivity', 'API\v1\QueryController@showProductivityScreen');
 Route::post('/admin/time-tracking-detail', 'API\v1\QueryController@getUserTimeTrackingDetail');
+ * 
+ */
