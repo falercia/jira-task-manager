716f75a0bc5c228ca007cc7d4835279518200cd3
Fabio Garcia <fabio.garcia@ypse.com.br>
2018-09-13 14:57:04 -0300

    More columns in non_compliance table

--- database/migrations/2018_09_13_142509_create_non_compliance_table.php
+++ database/migrations/2018_09_13_142509_create_non_compliance_table.php
@@ -12,12 +12,15 @@ class CreateNonComplianceTable extends Migration {
          $table->string('user_id', 100);
          $table->string('description', 2000);
          $table->date('date');
-         $table->enum('notified', ['Y', 'N']);
-         $table->date('notified_date');
+         $table->enum('notified', ['Y', 'N'])->default('N');
+         $table->date('notified_date')->nullable();
          $table->enum('severity', ['L', 'M', 'H']);
-         $table->string('action_plan', 2000);
-         $table->string('employee_comment', 2000);
-         $table->date('employee_comment_date');
+         $table->string('action_plan', 2000)->nullable();
+         $table->string('employee_comment', 2000)->nullable();
+         $table->date('employee_comment_date')->nullable();
+         $table->enum('type', ['AF', 'MP', 'DF', 'TF'])
+                 ->nullable()
+                 ->comment('AF: Attention Fault; MP: Merge Problem; DF: Development Fault; TF: Test Fault');
 
          $table->timestamps();
       });
