bb2e1bf3ad38f8edd3aaad4a92c4f05a662d1c0e
Fabio Garcia <fabio.garcia@ypse.com.br>
2018-09-13 11:33:30 -0300

    Add table to non_compliances register

---
+++ database/migrations/2018_09_13_142509_create_non_compliance_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNonComplianceTable extends Migration {
+
+   public function up() {
+      Schema::create('non_compliance', function (Blueprint $table) {
+         $table->increments('id');
+         $table->integer('user_id');
+         $table->string('description', 2000);
+         $table->date('date');
+         $table->enum('notified', ['Y', 'N']);
+         $table->date('notified_date');
+         $table->enum('severity', ['L', 'M', 'H']);
+         $table->string('action_plan', 2000);
+         $table->string('employee_comment', 2000);
+         $table->date('employee_comment_date');
+
+         $table->timestamps();
+      });
+   }
+
+   public function down() {
+      Schema::dropIfExists('non_compliance');
+   }
+
+}
